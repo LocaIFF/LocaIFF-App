fcbe729e6de4013754205dd264d48e9b92eb12e1
Julia Nunes Monteiro <julia.monteiro@gsuite.iff.edu.br>
2026-02-02 15:19:42 -0300

    Reestruturando

---
+++ app/src/main/java/br/iff/localiff/LocaliffApplication.java
@@ -0,0 +1,13 @@
+package br.iff.localiff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaliffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaliffApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/br/iff/localiff/LocaliffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.iff.localiff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaliffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
